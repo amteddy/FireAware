951c269019ca752c30e1da484e2c27e966878152
Tewodros W. Ambaw <teddy.ww@gmail.com>
2023-10-07 17:36:30 +0300

    Add FireAware mobile app

---
+++ FireAware/FireAware/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FireAware
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnReportWildFireClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ReportFirePage());
+        }
+
+        private void OnUseCurrentLocationClicked(object sender, EventArgs e) 
+        {
+            textField.Text = "California"; 
+            locationLabel.Text = !clearCheckbox.IsChecked? textField.Text : "No location info";
+        }
+    }
+}
+
---
+++ FireAware/FireAware/ReportFirePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FireAware
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ReportFirePage : ContentPage
+    {
+        public ReportFirePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnReportWildFireClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
